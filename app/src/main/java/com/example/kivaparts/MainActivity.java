1858047bfc1cd99a3b5845171b76b64ee06d8e65
Joe Castro <joecastroiv@hotmail.com>
2021-08-08 22:42:14 -0400

    Switched layout to a LinearLayout since pictures kept overlapping and this layout is cleaner for for user. Also added feature to lift motor imagebutton to open a new activity that displays information in regards to the part. End of the day

--- app/src/main/java/com/example/kivaparts/MainActivity.java
+++ app/src/main/java/com/example/kivaparts/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.kivaparts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton myImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImageButton = (ImageButton) findViewById(R.id.imageButton_liftMotor);
+
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, lift_motor_Activity.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+
+
     }
+
+
 }

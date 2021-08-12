e9cb871a1312c1246e16fee291628c70e8fc909a
Joe Castro <joecastroiv@hotmail.com>
2021-08-11 20:52:46 -0400

    Added checkboxes to lift motor pic when clicked to display what tools are needed

--- app/src/main/java/com/example/kivaparts/MainActivity.java
+++ app/src/main/java/com/example/kivaparts/MainActivity.java
@@ -2,28 +2,36 @@ package com.example.kivaparts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton myImageButton;
+    ImageButton myImageButton_liftMotor;
+    ImageButton myImageButton_mea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myImageButton = (ImageButton) findViewById(R.id.imageButton_liftMotor);
+        myImageButton_liftMotor = (ImageButton) findViewById(R.id.imageButton_liftMotor);
+        myImageButton_mea = (ImageButton) findViewById(R.id.imageButton_mea);
 
-        myImageButton.setOnClickListener(new View.OnClickListener() {
+        myImageButton_liftMotor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentLoadNewActivity = new Intent(MainActivity.this, lift_motor_Activity.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        myImageButton_mea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, mea_activity.class);
                 startActivity(intentLoadNewActivity);
             }
         });
